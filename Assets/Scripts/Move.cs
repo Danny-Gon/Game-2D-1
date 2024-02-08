dc316d345f8ed818a973ac556ed0d515a55ccb84
Danny-Gon <correodedanny@gmail.com>
2024-02-08 17:11:30 -0500

    Animaciones
    Se termina la animacion de Stand y Walk con el movimiento del jugador.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,18 +7,37 @@ public class Move : MonoBehaviour
     [SerializeField]private float speed;
 
     private Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spritePlayer;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        spritePlayer = GetComponentInChildren<SpriteRenderer>();
     }
 
 
     private void FixedUpdate()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
-        rb.velocity = new Vector2 (horizontal, vertical)* speed;
+
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+        animator.SetFloat("Walk", Mathf.Abs(rb.velocity.magnitude));
+
+        if (horizontal > 0)
+        {
+            spritePlayer.flipX = false;
+        }
+        else if (horizontal < 0)
+        {
+            spritePlayer.flipX = true;
+        }
     }
 }
