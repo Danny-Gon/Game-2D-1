b1eac3b3d913af27fbac3fc38189eeb86e4811fd
Danny-Gon <correodedanny@gmail.com>
2024-02-23 16:58:09 -0500

    Spawn y Timer
    Se modifica los spawnPoints, se agrega el timer de juego.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,78 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemy;
     public Transform[] spawnPoints;
-       
+    public int spawnCount = 10; //Contador de rondas
+    public float timeBetweenRounds = 5f;
 
-    private void Start()
+    private int spawnT = 0; //Ronda actual
+    //private bool spawning = false;
+
+    /*private void Start()
     {
-        print("Spawn Enemy");
-        
+        StartCoroutine(SpawnRounds());
+    }
+
+    private IEnumerator SpawnRounds()
+    {
+        spawning = true;
+        Debug.Log("Comenzando ronda " + (spawnT + 1));
+
         foreach (Transform spawnPoint in spawnPoints)
         {
-            Instantiate(enemy, spawnPoint);
+            for (int numero = 1; numero <= 2; numero++)
+            {
+                float numeroRandomX = Random.Range(0f, 0.8f);
+                float numeroRandomY = Random.Range(0f, 0.8f);
+
+                Vector3 offset = new Vector3(numeroRandomX, numeroRandomY, 0);
+
+                GameObject enemigo = Instantiate(enemy, spawnPoint);
+
+                enemigo.name = "Enemigo: " + (spawnT + 1) + "-" + numero;
+                enemigo.transform.position = enemigo.transform.position + offset;
+            }
         }
+
+        yield return new WaitForSeconds(timeBetweenRounds);
+        spawnT++;
     }
+
+}*/
+    private void Update()
+    {
+        
+        if(spawnT < spawnCount)
+        {
+            print("Se ha generado una oleada de enemigos");
+                                   
+            foreach (Transform spawnPoint in spawnPoints)
+            {
+                for(int indiceAuxiliar = 0; indiceAuxiliar < spawnPoint.childCount; indiceAuxiliar++)
+                {
+                    GameObject enemyT = spawnPoint.GetChild(indiceAuxiliar).gameObject;
+                }
+                
+                for (int numero = 1; numero <= 2; numero++)
+                {
+                    float numeroRandomX = Random.Range(0f, 0.8f);
+                    float numeroRandomY = Random.Range(0f, 0.8f);
+
+                    Vector3 offset = new Vector3(numeroRandomX, numeroRandomY, 0);
+
+                    GameObject enemigo = Instantiate(enemy, spawnPoint);
+                                                            
+                    enemigo.name = "Enemigo: " + spawnT + "-" + numero;
+                    enemigo.transform.position = enemigo.transform.position + offset;
+                }
+            }
+            spawnT++;
+        }        
+    }
+
+    private void LateUpdate()
+    {
+        spawnT = spawnCount;
+        //spawning = false;
+    }
+
 }
